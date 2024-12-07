5c64f6d2c86de0eed9195ab1344ff953fe4370fd
mahardika <sinaga@mahardikas-MacBook-Pro.local>
2024-12-07 14:52:08 +0700

    add timeout

--- src/MainScreen.tsx
+++ src/MainScreen.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable react/no-unstable-nested-components */
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import {
     Dimensions,
     FlatList,
@@ -10,20 +9,15 @@ import {
     TouchableOpacity,
     View,
 } from 'react-native';
+import Animated, { Extrapolate, interpolate, useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
+
 import image6 from './assets/images/6.jpg';
 import image7 from './assets/images/7.jpg';
 import image8 from './assets/images/8.jpg';
 import image9 from './assets/images/9.jpg';
 import image10 from './assets/images/10.jpg';
-import Animated, { Extrapolate, interpolate, useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
 
-const imageCollection = [
-    image6,
-    image7,
-    image8,
-    image9,
-    image10,
-];
+const imageCollection = [image6, image7, image8, image9, image10];
 
 const { width, height } = Dimensions.get('window');
 const CARD_WIDTH = width * 0.7;
@@ -32,23 +26,40 @@ const CARD_SPACING = 20;
 function MainScreen(): React.JSX.Element {
     const scrollX = useSharedValue(0);
     const [selectedCard, setSelectedCard] = useState<any>(null);
+    const [currentIndex, setCurrentIndex] = useState<number>(0);
+    const flatListRef = useRef<FlatList>(null);
 
-    const onCardPress = (item: any) => {
+    const onCardPress = (item: any, index: number) => {
         setSelectedCard(item);
+        setCurrentIndex(index);
     };
 
     const closeCard = () => {
         setSelectedCard(null);
+
+        setTimeout(() => {
+            flatListRef.current?.scrollToIndex({
+                index: currentIndex,
+                animated: true,
+            });
+        }, 750);
     };
 
-    let RenderItem = ({ data, index }: { data: string; index: number }) => {
+    const updateCurrentIndex = (event: any) => {
+        const offsetX = event.nativeEvent.contentOffset.x;
+        const index = Math.round(offsetX / (CARD_WIDTH + CARD_SPACING));
+        setCurrentIndex(index);
+    };
+
+    const RenderItem = ({ data, index }: { data: string; index: number }) => {
         const animatedStyle = useAnimatedStyle(() => {
             const scale = interpolate(
                 scrollX.value,
                 [
                     (index - 1) * (CARD_WIDTH + CARD_SPACING),
                     index * (CARD_WIDTH + CARD_SPACING),
-                    (index + 1) * (CARD_WIDTH + CARD_SPACING)],
+                    (index + 1) * (CARD_WIDTH + CARD_SPACING),
+                ],
                 [0.9, 1, 0.9],
                 Extrapolate.CLAMP
             );
@@ -57,7 +68,7 @@ function MainScreen(): React.JSX.Element {
 
         return (
             <Animated.View style={[styles.card, animatedStyle]} key={index}>
-                <Image source={data} resizeMethod='resize' style={styles.image} />
+                <Image source={data} resizeMethod="resize" style={styles.image} />
             </Animated.View>
         );
     };
@@ -74,6 +85,7 @@ function MainScreen(): React.JSX.Element {
             ) : (
                 <View style={styles.front}>
                     <FlatList
+                        ref={flatListRef}
                         data={imageCollection}
                         keyExtractor={(item, index) => index.toString()}
                         horizontal
@@ -82,13 +94,14 @@ function MainScreen(): React.JSX.Element {
                         snapToInterval={CARD_WIDTH + CARD_SPACING}
                         decelerationRate="fast"
                         renderItem={({ item, index }) => (
-                            <TouchableOpacity onPress={() => onCardPress(item)}>
-                                <RenderItem index={index} data={item} />;
+                            <TouchableOpacity onPress={() => onCardPress(item, index)}>
+                                <RenderItem index={index} data={item} />
                             </TouchableOpacity>
                         )}
                         onScroll={(event) => {
                             scrollX.value = event.nativeEvent.contentOffset.x;
                         }}
+                        onMomentumScrollEnd={updateCurrentIndex}
                         scrollEventThrottle={16}
                     />
                 </View>
